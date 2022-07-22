39d877b53caab8d07e65b84522e796f2da1aad14
Christian Escobar <cescobar852gmail.com>
2022-07-21 20:15:34 -0500

    add spinner visible

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("funciona")
+// console.log("funciona")
+
+
+
+
+// pintar el loading
+const loadingData = estado => {
+    const loading = document.getElementById("loading");
+    
+    if (estado) {
+        loading.classList.remove('d-none')
+    } else {
+        loading.classList.add('d-none');
+    }
+}
+
+
+loadingData(true);
